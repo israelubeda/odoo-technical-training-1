9e7c4493ada871490a36cc7a717e3bb96590da94
Israel Ubeda <israel.ubedabravo@gmail.com>
2022-05-10 23:49:43 +0000

    dia 2 modelos

---
+++ odoo_academia/models/biblioteca.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class biblioteca(models.Model):
+    _name = "libro"
+    _description = "Los bibliotecarios quieren registrar libros nuevos"
+    
---
+++ odoo_academia/models/curso.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class Curso(models.Model):
+    _name = "academia.curso"
+    _description = "Este es el curso de formacion tecnica odoo"
+    
