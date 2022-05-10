f49021f5481fc6cf7c7af4f1a1709b40177ab47f
Israel Ubeda <israel.ubedabravo@gmail.com>
2022-05-10 19:55:21 +0000

    ADD MODULO BASICO

---
+++ odoo_academia/__manifest__.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Odoo Academia',
+    
+    'summary': """App de prueba Academia""",
+
+    'description': """
+        mODULOS DE PRUEBAS:
+        - Cursos
+        - Sesiones
+        - Attendees
+        """,
+    
+    'author': 'Israel Ubeda Bravo',
+    
+    'website': 'https://israelubeda.github.io/',
+    
+    'category': 'Training',
+    'version': '15.0.1',
+    
+    'depends': ['sale'],
+    
+    'data': [
+        #'security/academy_security.xml',
+        #'security/ir.model.access.csv',
+        #'views/academy_menuitems.xml',
+        #'views/course_views.xml',
+        #'views/session_views.xml',
+        #'views/sales_views_inherit.xml',
+        #'views/product_views_inherit.xml',
+        #'views/academy_web_templates.xml',
+        #'wizard/sale_wizard_view.xml',
+        #'report/session_report_templates.xml',
+    ],
+    'demo': [
+        #'demo/academy_demo.xml',
+    ],
+    #Add license to remove License Warning
+    'license': 'OPL-1'
+}
