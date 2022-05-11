cfd56e70714531bffc9d57b3d99e260f817b2e72
Israel Ubeda <israel.ubedabravo@gmail.com>
2022-05-11 03:02:52 +0000

    dia 2 Campos

--- odoo_academia/models/curso.py
+++ odoo_academia/models/curso.py
@@ -6,3 +6,12 @@ class Curso(models.Model):
     _name = "academia.curso"
     _description = "Este es el curso de formacion tecnica odoo"
     
+    name = fields.Char(string="Titulo", required=True)
+    description = fields.Text(string="Descripcion :")
+    
+    nivel = fields.Selection(string="Nivel", selection = [('beginner', 'Novato'),
+                                       ('intermediate', 'Intermedio'),
+                                       ('advanced', 'Avanzado')],
+                            copy=False)
+    active = fields.Boolean(string="Activo", default=True)
+    
--- odoo_academia/models/nave_espacial.py
+++ odoo_academia/models/nave_espacial.py
@@ -6,3 +6,20 @@ class Nave(models.Model):
     _name = "nave.espacial"
     _description = "Para que Odoo llegue a la luna"
     
+    name = fields.Char(string="Nombre de la nave :", required=True)
+    description = fields.Text(string="Descripcion :")
+    dimen_largo = fields.Float(string="Largo :")
+    dimen_ancho = fields.Float(string="Ancho :")
+    dimen_altura = fields.Float(string="Altura :")
+    
+    tipo_combustible = fields.Selection(string="Tipo de combustible", selection = [('N', 'Nitro'),
+                                       ('D', 'Diesel'),
+                                       ('P', 'Petroleo')],
+                            copy=False)
+    tipo_barco = fields.Text(string="Tipo de barco :")
+    cant_pasajeros = fields.Float(string="Cantidad de Pasajeros :")
+    
+    
+    active = fields.Boolean(string="Activo", default=True)
+    
+    
