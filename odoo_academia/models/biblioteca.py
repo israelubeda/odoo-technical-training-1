c44b42fc77358bb9898663448c2cc820d161f547
Israel Ubeda <israel.ubedabravo@gmail.com>
2022-05-11 03:02:52 +0000

    dia 2 Campos

--- odoo_academia/models/biblioteca.py
+++ odoo_academia/models/biblioteca.py
@@ -6,3 +6,11 @@ class biblioteca(models.Model):
     _name = "libro"
     _description = "Los bibliotecarios quieren registrar libros nuevos"
     
+    name = fields.Char(string="Nombre del Libro :", required=True)
+    description = fields.Text(string="Descripcion :")
+    author = fields.Text(string="Autores :")
+    editor = fields.Text(string="Editores :")
+    editorial = fields.Text(string="Editorial :")
+    year = fields.Integer(string="Año de edicion :")
+    isbn = fields.Char(string="ISBN :")
+    genero = fields.Text(string="Genero :")    
--- odoo_academia/models/tarea.py
+++ odoo_academia/models/tarea.py
@@ -6,3 +6,13 @@ class tarea(models.Model):
     _name = "tarea"
     _description = "La cooperativa dividio todo el trabajo en tareas mas pequeñas"
     
+    name = fields.Char(string="Nombre de la tarea :", required=True)
+    description = fields.Text(string="Descripcion de la tarea :")
+    tipo = fields.Char(string="Tipo de tarea :")
+    
+    fecha_inicio = fields.Datetime(string="Fecha Inicio :")
+    fecha_final = fields.Datetime(string="Fecha Final :")
+    repite = fields.Boolean(string="Tarea repetida :", default=True)
+    frecuencia = fields.Integer(string="Frecuencia :")
+
+    
